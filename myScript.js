3b51578483ae4648fd50a0c0518d263cad4c53a3
Julia Peng <julia.peng10@gmail.com>
2017-01-18 12:38:40 -0800

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,32 @@
+var slideIndex = 1;
+
+function addSlides(n) {
+	showSlide(slideIndex += n);
+}
+
+function currentSlide(n) {
+	showSlide(slideIndex = n);
+}
+
+function showSlide(n) {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("fa-circle");
+	if(n > slides.length) {slideIndex = 1};
+	if(n < 1) {slideIndex = slides.length}
+	for(i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "")
+	}
+
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " active";
+	clearInterval(window);
+}
+
+window.setInterval(function showSlideA() {
+	addSlides(1);
+},5000);
